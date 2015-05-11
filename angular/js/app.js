271fae389cd556f537964b1b3b4b2e889695cdfc
Rajat Goyal <404rajat@gmail.com>
2015-05-11 14:01:58 +0530

    changes

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,7 +1,7 @@
 angular.module('singularApp',['ngRoute', 'mainController', 'mainService'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider) {
   $routeProvider.
-        when("/", {templateUrl: "views/signin.html", controller: "signinController"}).
+        when("/", {templateUrl: "views/signin.html"}).
         when("/:username/feed", {templateUrl: "views/feed.html", controller: "postController"}).
         when("/signup", {templateUrl: "views/signup.html", controller: "signupController"}).
         when("/signin", {templateUrl: "views/signin.html"}).
